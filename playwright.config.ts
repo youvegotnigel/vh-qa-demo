191f21d0bb21990575482ab47be0b254b2072504
youvegotnigel <nigeldave19@gmail.com>
2023-10-13 13:52:05 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,8 +32,17 @@ const RPconfig = {
  */
 export default defineConfig({
   testDir: './tests',
+  
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: './test-results',
+  preserveOutput: 'always',
 
-  timeout: 10*1000,
+   /* Maximum time expect() should wait for the condition to be met. */
+  timeout: 30*1000,
+
+  expect: {
+    timeout: 5*1000,
+},
 
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -45,15 +54,42 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 5,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-   reporter: 'html',
+  // reporter: 'html',
+
+  /* JUnit reporter produces a JUnit-style xml report. */
+  // reporter: [['junit', { outputFile: './test-results/results.xml' }]],
+
+  /* Blob reports contain all the details about the test run and can be used later to produce any other report. */
+  // reporter: [['blob', { outputDir: 'my-report' }]],
+
+  /* Concise 'dot' for CI, default 'list' when running locally */
+  // reporter: process.env.CI ? 'dot' : 'list',
   // reporter: [['@reportportal/agent-js-playwright', RPconfig]],
+
+    /* You can use multiple reporters at the same time */
+    reporter: [
+      ['list', { printSteps: true }],
+      ['html', { open: 'on-failure',  outputFolder: './test-results/html' }],
+      ['json', {  outputFile: './test-results/test-results.json' }],
+      ['junit', { outputFile: './test-results/results.xml' }],
+      // ['blob', { outputDir: './test-results/blob-report' }],
+      ['@reportportal/agent-js-playwright', RPconfig]
+    ],
+  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://the-internet.herokuapp.com/',
+
+    // headless: process.env.RUN_HEADLESS?.toLowerCase() === 'true',
+    // storageState: 'storageState.json',
+    actionTimeout: 10 * 1000,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -77,14 +113,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 7'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 14'] },
+    },
 
     /* Test against branded browsers. */
     // {
