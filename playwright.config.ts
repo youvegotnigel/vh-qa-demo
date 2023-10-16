e6955998a5201a6af804f32f383492381074353e
youvegotnigel <nigeldave19@gmail.com>
2023-10-16 14:06:32 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,6 +57,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 6,
 
+  testMatch: [/ui-test-demo.spec.ts/],
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
 
