060a32179fa849f96c44edd06634e0d9511f5057
youvegotnigel <nigeldave19@gmail.com>
2023-10-16 14:17:34 +0530

    Create applitools-vt-demo.spec.ts

---
+++ tests/applitools-vt-demo.spec.ts
@@ -0,0 +1,72 @@
+import { test } from '@playwright/test';
+import {
+    BatchInfo,
+    Configuration,
+    EyesRunner,
+    ClassicRunner,
+    Eyes,
+    Target
+} from '@applitools/eyes-playwright';
+
+const URL = 'file:///C:/dev/personal/playwright-projects/vh-qa-demo/riddle-me/riddle.html';
+const VIEWPORT_HEIGHT = 1200;
+const VIEWPORT_WIDTH = 1600;
+
+// Applitools
+export let Batch: BatchInfo;
+export let Config: Configuration;
+export let Runner: EyesRunner;
+let eyes: Eyes;
+// end of Applitools
+
+
+// beforeAll for Applitools
+test.beforeAll(async () => {
+
+    Runner = new ClassicRunner();
+    Batch = new BatchInfo({ name: `Riddles Demo Website - Classic Runner` });
+    Config = new Configuration();
+    Config.setApiKey("<API-KEY>");
+    Config.setBatch(Batch);
+});
+
+test.beforeEach(async ({ page }) => {
+    //Applitools
+    eyes = new Eyes(Runner, Config);
+    await eyes.open(
+        page,
+        'Riddles Demo',
+        test.info().title,
+        { width: VIEWPORT_WIDTH, height: VIEWPORT_HEIGHT }
+    );
+    //end of Applitools
+
+    await page.goto(URL);
+
+});
+
+
+test.afterEach(async () => {
+    await eyes.close();
+});
+
+
+test.afterAll(async () => {
+    // forces Playwright to wait synchronously for all visual checkpoints to complete.
+    const results = await Runner.getAllTestResults();
+    console.log('Visual test results', results);
+});
+
+
+test.describe('Applitools Visual Test Demo', () => {
+
+
+    test('Visual Comparison', async ({ page }) => {
+
+        await page.goto(URL);
+
+        await eyes.check('Riddle Page', Target.window().fully());
+
+    });
+
+});
