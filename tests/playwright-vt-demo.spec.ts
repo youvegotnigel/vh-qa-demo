399c080ccf74ccf3cad80bbd594e2fc80c339899
youvegotnigel <nigeldave19@gmail.com>
2023-10-16 16:35:38 +0530

    Update playwright-vt-demo.spec.ts

--- tests/playwright-vt-demo.spec.ts
+++ tests/playwright-vt-demo.spec.ts
@@ -6,8 +6,9 @@ test.describe('Playwright Visual Test Demo', () => {
 
     test('Visual Comparison', async ({ page }) => {
 
-        await page.goto('file:///C:/dev/personal/playwright-projects/vh-qa-demo/riddle-me/riddle.html')
-        //await page.goto('file://./riddle-me/riddle.html')
+        const filePath: string = `${process.cwd()}\\pages\\riddle.html`;
+
+        await page.goto(`file://${filePath}`);
         
         expect(await page.screenshot({
             fullPage: true
