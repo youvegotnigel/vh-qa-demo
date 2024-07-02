35ca08e539b3c178834972fa440c322003bcd2b3
Nigel <nigeldave19@gmail.com>
2024-07-02 11:28:49 +0530

    Update test-clock-api.spec.ts

--- tests/clock-api/test-clock-api.spec.ts
+++ tests/clock-api/test-clock-api.spec.ts
@@ -4,30 +4,6 @@ test.describe("Clock API Testing Demo", () => {
 
   const filePath: string = `${process.cwd()}\\pages\\clock.html`;
 
-  test("Set Fixed Time Test", async ({ page, }, testInfo) => {
-    
-    // Initialize clock with some time before the test time and let the page load
-    // naturally. `Date.now` will progress as the timers fire.
-    await page.clock.install({ time: new Date("2024-02-02T08:00:00") });
-    await page.goto(`file://${filePath}`);
-
-    // wait for content to load
-    await page.waitForLoadState("domcontentloaded");
-
-    // Pretend that the user closed the laptop lid and opened it again at 10am,
-    // Pause the time once reached that point.
-    await page.clock.pauseAt(new Date("2024-02-02T10:00:00"));
-
-    // Assert the page state.
-    await expect(page.getByTestId("current-time")).toHaveText("2/2/2024, 10:00:00 AM");
-
-    // Close the laptop lid again and open it at 10:30am.
-    await page.clock.fastForward("30:00");
-    await expect(page.getByTestId("current-time")).toHaveText("2/2/2024, 10:30:00 AM");
-  });
-
-
-
 
   test("set fixed time", async({ page,}, testInfo) => {
 
@@ -38,4 +14,21 @@ test.describe("Clock API Testing Demo", () => {
   });
 
 
+  test("manually advance time", async({ page,}, testInfo) => {
+
+    await page.clock.install({ time: new Date("2024-02-02T08:00:00") });
+    await page.goto(`file://${filePath}`);
+
+    await page.clock.pauseAt(new Date("2024-02-02T10:00:00"));
+    await expect(page.getByTestId("current-time")).toHaveText("2/2/2024, 10:00:00 AM");
+
+    await page.clock.fastForward("35:00");
+    await expect(page.getByTestId("current-time")).toHaveText("2/2/2024, 10:35:00 AM");
+
+  });
+
+
+
+
+  
 });
