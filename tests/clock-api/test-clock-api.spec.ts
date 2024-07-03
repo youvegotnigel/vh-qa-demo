face5868f81a2017bbaaab10d8d06ad3c0509659
youvegotnigel <nigeldave19@gmail.com>
2024-07-03 16:06:14 +0530

    Update test-clock-api.spec.ts

--- tests/clock-api/test-clock-api.spec.ts
+++ tests/clock-api/test-clock-api.spec.ts
@@ -28,6 +28,49 @@ test.describe("Clock API Testing Demo", () => {
   });
 
 
+  test("Synopsis advance time to verify logout", async({ page,}, testInfo) => {
+
+    await page.clock.install({ time: new Date("2024-02-02T08:00:00") });
+    await page.goto(`http://www.betterpreop.com/Log.do`);
+
+    // await page.clock.pauseAt(new Date("2024-02-02T10:00:00"));
+    // await expect(page.getByTestId("current-time")).toHaveText("2/2/2024, 10:00:00 AM");
+    await page.locator("[name=userName]").fill("AmilaS");
+    await page.locator("[name=password]").fill("amila@123");
+    await page.locator("[name=method]").click();
+
+    await expect(page.getByText('Hello Amila Silantha, welcome to Synopsis iQ')).toBeVisible()
+
+    await page.clock.fastForward("59:00");
+
+    await expect(page.getByText('Hello Amila Silantha, welcome to Synopsis iQ')).toBeVisible()
+
+  });
+
+
+  test("TREAT advance time to verify logout", async({ page,}, testInfo) => {
+
+    await page.clock.install({ time: new Date("2024-07-03T12:00:00") });
+    await page.goto(`http://vhvmdvtrtqaapp7.vh.local:8080/treat/logon`);
+
+
+    await page.locator("#orgUserID").fill("NSDCS");
+    await page.locator("#btnContinue").click();
+
+    await page.locator("#username").fill("hinext_automation");
+    await page.locator("#password").fill("password");
+    await page.getByRole('button', { name: 'Log On' }).click({ timeout: 60000 });
+
+
+    await expect(page.getByText('Automation User, NSDCS')).toBeVisible()
+
+    await page.clock.fastForward("30:00");
+
+    await expect(page.getByText('Automation User, NSDCS')).toBeVisible()
+
+  });
+
+
 
 
   
